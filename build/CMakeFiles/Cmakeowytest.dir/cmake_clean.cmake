dfb3146bb6f9b422864dd55587fd7324947e5300
Mistrzu <mar.traczynski@gmail.com>
2021-07-07 16:25:45 +0200

    first commit

---
+++ build/CMakeFiles/Cmakeowytest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cmakeowytest.dir/main.cpp.o"
+  "Cmakeowytest"
+  "Cmakeowytest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cmakeowytest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include "matplotlibcpp.h"
+#include <vector>
+
+namespace plt = matplotlibcpp;
+
+int main() {
+  std::vector<double> y = {1, 3, 2, 4};
+  //plt::figure();
+  plt::plot(y);
+  plt::savefig("minimal.pdf");
+}
